f0efe8cdbfe3047e6276a8a10518df39763d0170
jincheng9 <perfume0607@gmail.com>
2023-03-25 18:48:52 +0800

    update

--- workspace/openai/chatgpt.py
+++ workspace/openai/chatgpt.py
@@ -6,7 +6,7 @@ print(openai.Model.list())
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "测试一下"}
+    {"role": "system", "content": "自我介绍下"}
   ]
 )
 
