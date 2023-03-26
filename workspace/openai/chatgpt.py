3b46398a1f5ac605b6cab54461ebae0377c9f1d2
jincheng9 <perfume0607@gmail.com>
2023-03-26 23:27:08 +0800

    update

--- workspace/openai/chatgpt.py
+++ workspace/openai/chatgpt.py
@@ -1,13 +1,18 @@
 import os
+import time
 import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print(openai.Model.list())
+print(openai.Model.retrieve("gpt-3.5-turbo"))
+
+start_time = time.time()
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "自我介绍下"}
-  ]
+    {"role": "system", "content": "为什么喜欢狗"}
+  ],
 )
-
+end_time = time.time()
+print("time cost:", end_time-start_time)
 print(completion.choices[0].message.content)
+print(completion.usage.total_tokens)
